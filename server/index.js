3d7510ea82b8f3fcdeea11a18cd984f4733fdacf
Reid Goodbar <reidgoodbar@uchicago.edu>
2022-01-21 02:26:53 -0600

    updated read me

--- server/index.js
+++ server/index.js
@@ -4,6 +4,15 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
 
 app.use(express.static(path.resolve(__dirname, '../rps_client/build')));
 
